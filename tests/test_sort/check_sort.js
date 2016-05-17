c87208463e0603eb17b99c0b9a2835b56ad339c8
Alexander Lisianoi <all3fox@gmail.com>
2016-05-17 17:36:45 +0200

    Core: make check_sort.js standard

--- tests/test_sort/check_sort.js
+++ tests/test_sort/check_sort.js
@@ -1,97 +1,92 @@
-var assert = require("assert");
-var random = require("seedrandom");
+let assert = require('assert')
+let random = require('seedrandom')
 
-function issorted(xs) {
-	for (var i = 1; i < xs.length; ++i) {
-		if (xs[i - 1] > xs[i])
-			return false;
-	}
+function issorted (xs) {
+  for (let i = 1; i < xs.length; ++i) {
+    if (xs[i - 1] > xs[i]) { return false }
+  }
 
-	return true;
+  return true
 }
 
-function irandom(min, max) {
-	return Math.floor(Math.random() * (max - min)) + min;
+function irandom (min, max) {
+  return Math.floor(Math.random() * (max - min)) + min
 }
 
-function check_on_empty(sort) {
-	it(sort.name + ', inplace array', function() {
-		var xs = sort([]);
-		assert.equal(xs === undefined || xs.length === 0, true);
-	});
+function check_on_empty (sort) {
+  it(sort.name + ', inplace array', function () {
+    let xs = sort([])
+    assert.equal(xs === undefined || xs.length === 0, true)
+  })
 
-	it(sort.name + ', separate array', function() {
-		var xs = [];
+  it(sort.name + ', separate array', function () {
+    let xs = []
 
-		var ys = sort(xs);
-		if (ys === undefined) {
-			assert.deepEqual(xs, []);
-		} else {
-			assert.deepEqual(xs, []);
-			assert.deepEqual(ys, []);
-		}
-	});
+    let ys = sort(xs)
+    if (ys === undefined) {
+      assert.deepEqual(xs, [])
+    } else {
+      assert.deepEqual(xs, [])
+      assert.deepEqual(ys, [])
+    }
+  })
 }
 
-function check_on_sorted(sort, n, reversed) {
-	var xs = [];
-	if (!reversed) {
-		for (var i = 0; i < n; ++i)
-			xs[i] = i;
-	} else {
-		for (var i = n - 1; i >= 0; --i)
-			xs[i] = i;
-	}
+function check_on_sorted (sort, n, reversed) {
+  let xs = []
+  if (!reversed) {
+    for (let i = 0; i < n; ++i) { xs[i] = i }
+  } else {
+    for (let i = n - 1; i >= 0; --i) { xs[i] = i }
+  }
 
-	var ys = [];
-	for (var i = 0; i < n; ++i)
-		ys[i] = i;
+  let ys = []
+  for (let i = 0; i < n; ++i) { ys[i] = i }
 
-	var zs = xs.slice();
+  let zs = xs.slice()
 
-	var rs = sort(xs);
+  let rs = sort(xs)
 
-	if (rs === undefined) {
-		it(sort.name + ", length " + n, function() {
-			assert.deepEqual(xs, ys);
+  if (rs === undefined) {
+    it(sort.name + ', length ' + n, function () {
+      assert.deepEqual(xs, ys)
 
-			assert.equal(issorted(xs), true);
-		});
-	} else {
-		it(sort.name + ", length " + n, function() {
-			assert.deepEqual(xs, zs);
-			assert.deepEqual(rs, ys);
+      assert.equal(issorted(xs), true)
+    })
+  } else {
+    it(sort.name + ', length ' + n, function () {
+      assert.deepEqual(xs, zs)
+      assert.deepEqual(rs, ys)
 
-			assert.equal(issorted(rs), true);
-		});
-	}
+      assert.equal(issorted(rs), true)
+    })
+  }
 }
 
+function check_on_random (sort, n, seed) {
+  random(seed, {global: true})
 
-function check_on_random(sort, n, seed) {
-	random(seed, {global: true});
+  let [xs, ys] = [[], []]
+  for (let i = 0; i < n; ++i) {
+    xs[i] = irandom(-1024, 1024)
+  }
 
-	var xs = [], ys = [];
-	for (var i = 0; i < n; ++i) {
-		xs[i] = irandom(-1024, 1024);
-	}
+  let zs = xs.slice()
 
-	var zs = xs.slice();
+  ys = sort(xs)
 
-	ys = sort(xs);
-
-	if (ys === undefined) {
-		it(sort.name + ", length " + n, function() {
-			assert.equal(issorted(xs), true);
-		});
-	} else {
-		it(sort.name + ", length " + n, function() {
-			assert.deepEqual(xs, zs);
-			assert.equal(issorted(ys), true);
-		});
-	}
+  if (ys === undefined) {
+    it(sort.name + ', length ' + n, function () {
+      assert.equal(issorted(xs), true)
+    })
+  } else {
+    it(sort.name + ', length ' + n, function () {
+      assert.deepEqual(xs, zs)
+      assert.equal(issorted(ys), true)
+    })
+  }
 }
 
-exports.check_on_empty  = check_on_empty;
-exports.check_on_sorted = check_on_sorted;
-exports.check_on_random = check_on_random;
+exports.check_on_empty = check_on_empty
+exports.check_on_sorted = check_on_sorted
+exports.check_on_random = check_on_random
