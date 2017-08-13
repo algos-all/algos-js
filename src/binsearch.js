7f8e7a8119512917208c26d25de91dc4f8970f48
Alexander Lisianoi <all3fox@gmail.com>
2017-08-13 13:27:22 +0200

    Core: add lower- and upper_bound and test their simple cases

--- src/binsearch.js
+++ src/binsearch.js
@@ -18,4 +18,52 @@ function binsearch (xs, v) {
     return null
 }
 
+function lower_bound(xs, val, compare) {
+    if (xs === undefined || val === undefined) {
+        return null
+    }
+
+    if (compare === undefined) {
+        compare = (x, y) => {return x < y}
+    }
+
+    let [fst, lst] = [0, xs.length - 1]
+    while (fst <= lst) {
+        const mid = fst + (lst - fst) / 2 | 0
+
+        if (compare(xs[mid], val)) {
+            fst = mid + 1
+        } else {
+            lst = mid - 1
+        }
+    }
+
+    return fst
+}
+
+function upper_bound(xs, val, compare) {
+    if (xs === undefined || val === undefined) {
+        return null
+    }
+
+    if (compare === undefined) {
+        compare = (x, y) => {return x <= y}
+    }
+
+    let [fst, lst] = [0, xs.length - 1]
+    while (fst <= lst) {
+        const mid = fst + (lst - fst) / 2 | 0
+
+        if (compare(xs[mid], val)) {
+            fst = mid + 1
+        } else {
+            lst = mid - 1
+        }
+    }
+
+    return fst
+}
+
 exports.binsearch = binsearch
+exports.lower_bound = lower_bound
+exports.upper_bound = upper_bound
--- test/test_binsearch.js
+++ test/test_binsearch.js
@@ -36,3 +36,86 @@ describe('Test binsearch', function () {
         }
     })
 })
+
+describe('Test lower_bound', function() {
+    let lower_bound = module.lower_bound
+
+    it(lower_bound.name + ' on an empty array ', function() {
+        assert.equal(lower_bound([], 42), 0)
+    })
+
+    it(lower_bound.name + ' on a single element', function() {
+        assert.equal(lower_bound([42], 40), 0)
+        assert.equal(lower_bound([42], 42), 0)
+        assert.equal(lower_bound([42], 44), 1)
+    })
+
+    describe(lower_bound.name + ' on a sorted array', function() {
+        let xs = []
+
+        for (let i = 0; i !== 50; ++i) {
+            xs[i] = 2 * i
+
+            for (let j = 0; j !== i; ++j) {
+                check_bsearch(lower_bound, xs, 2 * j, j)
+                check_bsearch(lower_bound, xs, 2 * j + 1, j + 1)
+
+                check_bsearch(lower_bound, xs, -1  , 0)
+                check_bsearch(lower_bound, xs, 1000, xs.length)
+            }
+        }
+    })
+
+    it(lower_bound.name + ' on continuous', function() {
+        let xs = [-2, -2, -2, 2, 2, 2, 2]
+
+        assert.equal(lower_bound(xs.slice(), -3), 0)
+        assert.equal(lower_bound(xs.slice(), -2), 0)
+        assert.equal(lower_bound(xs.slice(), -1), 3)
+        assert.equal(lower_bound(xs.slice(),  0), 3)
+        assert.equal(lower_bound(xs.slice(),  2), 3)
+        assert.equal(lower_bound(xs.slice(), 10), xs.length)
+    })
+})
+
+describe('Test upper_bound', function() {
+    let upper_bound = module.upper_bound
+
+    it(upper_bound.name + ' on an empty array ', function() {
+        assert.equal(upper_bound([], 42), 0)
+    })
+
+    it(upper_bound.name + ' on a single element', function() {
+        assert.equal(upper_bound([42], 40), 0)
+        assert.equal(upper_bound([42], 42), 1)
+        assert.equal(upper_bound([42], 44), 1)
+    })
+
+    describe(upper_bound.name + ' on a sorted array', function() {
+        let xs = []
+
+        for (let i = 0; i !== 50; ++i) {
+            xs[i] = 2 * i
+
+            for (let j = 0; j !== i; ++j) {
+                check_bsearch(upper_bound, xs, 2 * j - 1, j)
+                check_bsearch(upper_bound, xs, 2 * j, j + 1)
+
+
+                check_bsearch(upper_bound, xs, -1  , 0)
+                check_bsearch(upper_bound, xs, 1000, xs.length)
+            }
+        }
+    })
+
+    it(upper_bound.name + ' on continuous', function() {
+        let xs = [-2, -2, -2, 2, 2, 2, 2]
+
+        assert.equal(upper_bound(xs.slice(), -3), 0)
+        assert.equal(upper_bound(xs.slice(), -2), 3)
+        assert.equal(upper_bound(xs.slice(), -1), 3)
+        assert.equal(upper_bound(xs.slice(),  0), 3)
+        assert.equal(upper_bound(xs.slice(),  2), xs.length)
+        assert.equal(upper_bound(xs.slice(), 10), xs.length)
+    })
+})
